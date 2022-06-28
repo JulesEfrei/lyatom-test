de7acd50aa967f623e05fbef8cc117f7ae6ebb8e
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-06-28 12:11:12 +0200

    Fix

--- src/lyatomConfig.ts
+++ src/lyatomConfig.ts
@@ -4,14 +4,14 @@ const data: CmsPropsType = {
   branch: "main", // Branch to update
   repo: "JulesEfrei/lyatom-test", // Follows the pattern [org-or-username]/[repo-name]
   urlForLogin: "admin", // url for access of the panelAdmin
-  mediaFolder: "/assets/", // root path to the media files
+  mediaFolder: "/src/assets/", // root path to the media files
   title: "Test Lyatom CMS", // title in the PanelAdmin
   type: "firstLvl", // start always with this
   fields: [
     {
       type: "section", // start second with this its to create first section on Panel
       title: "Home",
-      file: "/data/data.json", // root path specifies info of the first section
+      file: "/src/data/data.json", // root path specifies info of the first section
       fields: [
         {
           type: "input",
@@ -19,11 +19,18 @@ const data: CmsPropsType = {
           id: "paragraph", // key reference in the file ( info.json)
           htmlId: "paragraph", // id to write in the html for real time editing
         },
+      ],
+    },
+    {
+      type: "section", // start second with this its to create first section on Panel
+      title: "Test",
+      file: "/src/data/data.json", // root path specifies info of the first section
+      fields: [
         {
           type: "input",
-          title: "Link Text",
-          id: "link.text",
-          htmlId: "link-test",
+          title: "Paragraph", // printing on AdminPanel
+          id: "paragraph", // key reference in the file ( info.json)
+          htmlId: "paragraph", // id to write in the html for real time editing
         },
       ],
     },
